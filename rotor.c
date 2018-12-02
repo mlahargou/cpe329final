783c9505b19039e09fb5fe3c0327c4fcfbe0bacc
Michael Lahargou <michaellahargou@gmail.com>
2018-12-01 19:34:39 -0800

    Add reflector and rotor backtrack

--- rotor.c
+++ rotor.c
@@ -1,14 +1,12 @@
 #include "rotor.h"
 
-
-
 void rotateRotor(int rotorArray[], int rotateAmount) {
     signed int tempHolder;
     int i, j;
     if (rotateAmount > 0) {  //shifting to the right
         for (i = 0; i < rotateAmount; i++) { //shift rotateAmount times
-            tempHolder = rotorArray[25];     //hold the last value so it does not get overwritten
-            for(j = 24; j > -1; j--) {        //shift the whole array one time
+            tempHolder = rotorArray[ROTORSIZE-1];     //hold the last value so it does not get overwritten
+            for(j = ROTORSIZE - 2; j > -1; j--) {        //shift the whole array one time
                 rotorArray[j + 1] = rotorArray[j];
             }
             rotorArray[0] = tempHolder;      //add the temp variable back to the front
@@ -17,10 +15,10 @@ void rotateRotor(int rotorArray[], int rotateAmount) {
     else if (rotateAmount < 0) { //shifting to the left
         for (i = 0; i < -rotateAmount; i++) {
             tempHolder = rotorArray[0];
-            for(j = 0; j < 25; j++) {
+            for(j = 0; j < (ROTORSIZE - 1); j++) {
                 rotorArray[j] = rotorArray[j+1];
             }
-            rotorArray[25] = tempHolder;
+            rotorArray[ROTORSIZE - 1] = tempHolder;
         }
     }
 }
@@ -29,19 +27,49 @@ int travelThroughRotor(int rotorArray[], int index) {
 
     signed int newIndex = fromAtoB + index;
 
-    if (newIndex >= 0 && newIndex <= 25) {
+    if (newIndex >= 0 && newIndex <= (ROTORSIZE - 1)) {
         return newIndex;
     }
 
     if (newIndex < 0) {  //newIndex is negative
-        return newIndex + 26;  //wrap around to the higher index
+        return newIndex + ROTORSIZE;  //wrap around to the higher index
     }
-    return newIndex - 26;
+    return newIndex - ROTORSIZE;
 }
 
-int travelIndexThroughAllRotors(int rotorArrayA[], int rotorArrayB[], int rotorArrayC[], int index) {
-    int indexA = travelThroughRotor(rotorArrayA, 5);
-    int indexB = travelThroughRotor(rotorArrayB, indexA);
-    return travelThroughRotor(rotorArrayC, indexB);
+int travelIndexThroughForwardRotors(Rotors allRotors, int index) {
+    int indexA = travelThroughRotor(allRotors.rotor1, 5);
+    int indexB = travelThroughRotor(allRotors.rotor2, indexA);
+    return travelThroughRotor(allRotors.rotor3, indexB);
+}
+
+int travelIndexThroughReflector(Rotors allRotors, int index) {
+    return travelThroughRotor(allRotors.reflector, index);
+}
+
+int travleIndexThroughBackwardRotors(Rotors allRotors, int index) {
+    int indexZ = travelThroughRotor(allRotors.rotor5, 5);
+    int indexY = travelThroughRotor(allRotors.rotor6, indexZ);
+    return travelThroughRotor(allRotors.rotor7, indexY);
+}
+
+int travelIndexThroughAllRotors(Rotors allRotors, int index) {
+    int indexForward = travelIndexThroughForwardRotors(allRotors, index);
+    int indexReflector = travelIndexThroughReflector(allRotors, indexForward);
+    return travleIndexThroughBackwardRotors(allRotors, indexReflector);
+}
+
+char getCharByIndex(int index){
+    return 65 + index;
+}
+
+char getIndexByChar(char character) {
+    return character - 65;
+}
+
+char getCharThroughAllRotors(Rotors allRotors, char startLetter) {
+    int startIndex = getIndexByChar(startLetter);
+    int endIndex = travelIndexThroughAllRotors(allRotors, startIndex);
+    return getCharByIndex(endIndex);
 }
 
--- rotor.h
+++ rotor.h
@@ -8,10 +8,26 @@
 #ifndef ROTOR_H_
 #define ROTOR_H_
 
+#define ROTORSIZE 26//95
+
+typedef struct {
+    signed int rotor1[ROTORSIZE];
+    signed int rotor2[ROTORSIZE];
+    signed int rotor3[ROTORSIZE];
+    signed int reflector[ROTORSIZE];
+    signed int rotor5[ROTORSIZE];
+    signed int rotor6[ROTORSIZE];
+    signed int rotor7[ROTORSIZE];
+} Rotors;
 
 void rotateRotor(int* rotorArray, int rotateAmount);
 int travelThroughRotor(int rotorArray[], int index);
-int travelIndexThroughAllRotors(int rotorArrayA[], int rotorArrayB[], int rotorArrayC[], int index);
-
+int travelIndexThroughForwardRotors(Rotors allRotors,  int index);
+int travelIndexThroughReflector(Rotors allRotors,  int index);
+int travleIndexThroughBackwardRotors(Rotors allRotors,  int index);
+int travelIndexThroughAllRotors(Rotors allRotors, int index);
+char getCharThroughAllRotors(Rotors allRotors, char startLetter);
+char getCharByIndex(int index);
+char getCharThroughAllRotors(Rotors allRotors, char startLetter);
 
 #endif /* ROTOR_H_ */
